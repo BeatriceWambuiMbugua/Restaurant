fe75946b6b1b76f01f9a27b1c2c7e5f45d4a5ecb
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2022-05-12 15:51:09 +0300

    create RestaurantActivity

--- app/src/main/java/com/digitalspaceguru/restaurant/MainActivity.java
+++ app/src/main/java/com/digitalspaceguru/restaurant/MainActivity.java
@@ -3,12 +3,25 @@ package com.digitalspaceguru.restaurant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantsButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate(Bundle savedInstanceState) { //is called every time a user visits this activity.
+        super.onCreate(savedInstanceState); //causes Android to run all of the default behaviors for an activity.
+        setContentView(R.layout.activity_main); //setContentView tells the activity which layout to use for the device screen.
+        //R.layout.activity_main tells Android to use the main_activity.xml layout for this activity. R - which is short for Resources - gives us a way to access all of our files in the res directory.
+    mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+
+    mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_LONG).show();
+        }
+    });
     }
 }
