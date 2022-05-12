96f7269c1cc1ebbd6fe78112e4783cdd82694334
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2022-05-12 17:14:41 +0300

    add editText in the MainActivity

--- app/src/main/java/com/digitalspaceguru/restaurant/MainActivity.java
+++ app/src/main/java/com/digitalspaceguru/restaurant/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { //is called every time a user visits this activity.
@@ -17,10 +21,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main); //setContentView tells the activity which layout to use for the device screen.
         //R.layout.activity_main tells Android to use the main_activity.xml layout for this activity. R - which is short for Resources - gives us a way to access all of our files in the res directory.
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
 
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String location = mLocationEditText.getText().toString(); //getText() method returns an editable data type so we have to convert it to a String using the toString() method
+                Log.d(TAG, location);
                 // Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
                 startActivity(intent);
