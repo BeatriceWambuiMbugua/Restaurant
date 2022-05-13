0589b74bbdec6facd90204cc097dc170037806bb
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2022-05-13 08:46:38 +0300

    implement an arrayadapter

--- app/src/main/java/com/digitalspaceguru/restaurant/RestaurantsActivity.java
+++ app/src/main/java/com/digitalspaceguru/restaurant/RestaurantsActivity.java
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class RestaurantsActivity extends AppCompatActivity {
     private TextView mlocationTextView;
     private ListView mListView;
-    private String[] restaurant = new String[]{"Mi Mero Mole", "Mother's Bistro",
+    private String[] restaurants = new String[]{"Mi Mero Mole", "Mother's Bistro",
             "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
             "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
             "Lardo", "Portland City Grill", "Fat Head's Brewery",
@@ -22,6 +23,9 @@ public class RestaurantsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants);
         mlocationTextView = (TextView) findViewById(R.id.locationTextView);
         mListView = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
+        mListView.setAdapter(adapter);
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mlocationTextView.setText("Here are the restaurants near the place: " + location);//call the setText() method on mLocationTextView to update its text to contain the sentence we include, and the location string we previously defined.
